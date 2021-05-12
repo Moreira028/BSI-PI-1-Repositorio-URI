f0106db3d00b88664faf84cedf598dc340af67b7
Gabriel Ferreira <moreira028@gmail.com>
2021-05-12 19:52:20 -0300

    Exercício - Hello World / Gabriel Ferreira

---
+++ lista_01/exercicio_1000.java
@@ -0,0 +1,12 @@
+# Exercício - Hello World
+
+public class Main {       
+        public static void main(String[] args) {
+            System.out.println("Hello World!");
+    
+        }    
+}
+
+Ferreira, Gabriel. moreira028@.gmail.com
+
+Data: 12/05/2021
